5ebb6d37ff1412dd2b92ccbcfaaea42f8b7a1400
Beau Elliott <belliott1516@gmail.com>
2022-08-24 11:54:37 -0700

    changed items to goals and adapted items route and models to goals

---
+++ tests/goals.test.js
@@ -0,0 +1,7 @@
+// const request = require('supertest');
+const { setupDb } = require('./utils.js');
+// const app = require('../lib/app');
+
+describe('/api/v1/items', () => {
+  beforeEach(setupDb);
+});
