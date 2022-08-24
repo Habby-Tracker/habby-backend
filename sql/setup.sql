a628abf9e0a3069de966202ab83ff8f27844dcc7
Beau Elliott <belliott1516@gmail.com>
2022-08-24 11:06:15 -0700

    added goal table and habits table to setupsql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS habits;
+DROP TABLE IF EXISTS goals;
 DROP TABLE IF EXISTS users;
+
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -11,10 +13,32 @@ CREATE TABLE users (
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE items (
+CREATE TABLE goals (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT,
-    description VARCHAR NOT NULL,
+    user_id BIGINT NOT NULL,
+    goal_category_id BIGINT NOT NULL,
+    goal_name VARCHAR NOT NULL,
+    time_period_id INT NOT NULL,
+    habit_type_id VARCHAR NOT NULL,
+    default_habit_name VARCHAR NOT NULL,
+    status_id BIGINT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (status_id) REFERENCES status(id),
+    FOREIGN KEY (goal_category_id) REFERENCES categories(id),
+    FOREIGN KEY (time_period_id) REFERENCES time_periods(id),
+    FOREIGN KEY (habit_type_id) REFERENCES habit_types(id)
 );
+
+CREATE TABLE habits (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    goal_id BIGINT NOT NULL, 
+    user_id BIGINT NOT NULL,
+    habit_name VARCHAR NOT NULL,
+    status_id BIGINT NOT NULL,
+    due_date TIMESTAMP NOT NULL,
+    completed_date TIMESTAMP NOT NULL
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (status_id) REFERENCES status(id),
+    FOREIGN KEY (goal_id) REFERENCES goals(id)
+)
