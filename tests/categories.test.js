3934018d89b04003a8003182e8fc05faedb8c4f5
Beau Elliott <belliott1516@gmail.com>
2022-08-24 15:26:53 -0700

    added post test to categories

--- tests/categories.test.js
+++ tests/categories.test.js
@@ -2,6 +2,8 @@
 const { setupDb, signUpUser } = require('./utils.js');
 // const app = require('../lib/app');
 
+const newCategory = { name: 'Other', defaultIcon: 'https://i.imgur.com/0Z0Z7Z0.png', user_id: null };
+
 describe('/api/v1/items', () => {
   beforeEach(setupDb);
 
@@ -13,10 +15,16 @@ describe('/api/v1/items', () => {
     expect(body.length).toEqual(10);
   });
 
-  //   it('GET / should return a 401 if not authenticated', async () => {
-  //     const { status } = await request(app).get('/api/v1/goals');
-  //     expect(status).toEqual(401);
-  //   });
+  it('POST / adds a new category for the user', async () => {
+    const { agent } = await signUpUser();
+    const { status, body } = await agent.post('/api/v1/categories').send(newCategory);
+
+    expect(status).toEqual(200);
+    expect(body).toEqual({
+      ...newCategory,
+      id: expect.any(String)
+    });
+  });
 
 
 
