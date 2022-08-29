0fb9c6fe855db6aaff35ba396a7833e1b1575002
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-29 14:57:55 -0700

    fix habit tests to account for status name being on Habit model

--- tests/habits.test.js
+++ tests/habits.test.js
@@ -1,11 +1,20 @@
 const request = require('supertest');
-const {
-  setupDb,
-  signUpUser
-} = require('./utils.js');
+const { setupDb, signUpUser } = require('./utils.js');
 const app = require('../lib/app');
-const newHabit = { goalID:'1', habitName: 'read', statusID: '1', dueDate: '2022-03-23T20:30:00.000Z', completedDate:'2022-03-24T20:30:00.000Z' };
-const newHabit2 = { goalID:'2', habitName: 'eat veggies', statusID: '1', dueDate: '2022-03-29 13:30:00', completedDate:'2022-03-30 13:30:00' };
+const newHabit = {
+  goalID: '1',
+  habitName: 'read',
+  statusID: '1',
+  dueDate: '2022-03-23T20:30:00.000Z',
+  completedDate: '2022-03-24T20:30:00.000Z',
+};
+const newHabit2 = {
+  goalID: '2',
+  habitName: 'eat veggies',
+  statusID: '1',
+  dueDate: '2022-03-29 13:30:00',
+  completedDate: '2022-03-30 13:30:00',
+};
 
 describe('/api/v1/items', () => {
   beforeEach(setupDb);
@@ -20,28 +29,32 @@ describe('/api/v1/items', () => {
       id: expect.any(String),
       user_id: user.id,
       dueDate: expect.any(String),
-      completedDate: expect.any(String)
+      completedDate: expect.any(String),
     });
   });
 
   it('GET / returns all items associated with the authenticated User', async () => {
     const { agent } = await signUpUser();
-    const { body: user1Habit } = await agent.post('/api/v1/habits').send(newHabit);
+    const { body: user1Habit } = await agent
+      .post('/api/v1/habits')
+      .send(newHabit);
 
     const { agent: agent2 } = await signUpUser({
       email: 'user2@email.com',
       password: 'password',
     });
 
-    const { body: user2Habit } = await agent2.post('/api/v1/habits').send(newHabit2);
+    const { body: user2Habit } = await agent2
+      .post('/api/v1/habits')
+      .send(newHabit2);
 
     const resp1 = await agent.get('/api/v1/habits');
     expect(resp1.status).toEqual(200);
-    expect(resp1.body).toEqual([user1Habit]);
+    expect(resp1.body).toEqual([{ ...user1Habit, status: 'Active' }]);
 
     const resp2 = await agent2.get('/api/v1/habits');
     expect(resp2.status).toEqual(200);
-    expect(resp2.body).toEqual([user2Habit]);
+    expect(resp2.body).toEqual([{ ...user2Habit, status: 'Active' }]);
   });
 
   it('GET /:id should get a habit', async () => {
@@ -51,7 +64,7 @@ describe('/api/v1/items', () => {
     const { status, body: got } = await agent.get(`/api/v1/habits/${habit.id}`);
 
     expect(status).toBe(200);
-    expect(got).toEqual(habit);
+    expect(got).toEqual({ ...habit, status: 'Active' });
   });
 
   it('GET / should return a 401 if not authenticated', async () => {
@@ -108,5 +121,3 @@ describe('/api/v1/items', () => {
   });
 });
 //small change
-
-
